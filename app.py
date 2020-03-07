2dfe84d58559481ecd8e0847a8d1907e9bd0fe96
Mars <marcsand079@gmail.com>
2020-03-07 20:07:43 -0300

    Fix database errors

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -17,7 +16,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine("postgres://eihlqaagoqafiz:b88014c70e4195f1e91ef436da25435bf45d4986ea49728d363a90bc64931d12@ec2-107-22-228-141.compute-1.amazonaws.com:5432/d8u7rj319n10u6")
+engine = create_engine("postgres://sipkembfnzzxvw:09f9dd2e9f7e4ad36386017bb7456a95decb84a33031ae202e9c349506243e44@ec2-18-213-176-229.compute-1.amazonaws.com:5432/d6mhpgkf7uih34")
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route('/')
